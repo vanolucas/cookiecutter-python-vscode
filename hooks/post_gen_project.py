99b2d00b03db4f99fd0e5e54fa2d7eb42af7842a
vanolucas <vanolucas@gmail.com>
2023-12-03 00:51:44 +0100

    feat: .env file generation by cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,4 @@
+import re
 from os import path
 from shutil import copyfile
 
@@ -6,8 +7,4 @@ PROJECT_DIRECTORY = path.realpath(path.curdir)
 
 
 if __name__ == '__main__':
-    # Create a .env file from the sample
-    env_filename = ".env"
-    src = path.join(PROJECT_DIRECTORY, f"{env_filename}-sample")
-    dst = path.join(PROJECT_DIRECTORY, env_filename)
-    copyfile(src, dst)
+    pass
