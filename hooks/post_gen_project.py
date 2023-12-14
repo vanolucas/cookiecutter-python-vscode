e1c5127f8066f09d57327f9acf42692806dc4f1e
vanolucas <vanolucas@gmail.com>
2023-12-15 00:50:02 +0100

    Various improvements (#1)
    * feat: various small improvements
    
    * feat: small improvements
    
    * chore(sample proj): remove demos
    
    * chore(cookiecutter): update default description
    
    * feat(sample proj): added mermaid vscode extensions

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,9 +5,9 @@ PROJECT_DIRECTORY = path.realpath(path.curdir)
 
 
 if __name__ == "__main__":
-    # Copy .env file from root to .devcontainer/
+    # Copy .env file from .devcontainer/ to root
     # Reason: newer docker-compose versions look for the .env file next to
     # the docker-compose.yml file
     root_env_path = path.join(PROJECT_DIRECTORY, ".env")
     devcontainer_env_path = path.join(PROJECT_DIRECTORY, ".devcontainer/.env")
-    copyfile(root_env_path, devcontainer_env_path)
+    copyfile(devcontainer_env_path, root_env_path)
